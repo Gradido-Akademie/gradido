4144e6e27ceb43c8622d0a0b86a2b5bf62e2973e
einhornimmond <info@einhornimmond.de>
2026-07-02 14:37:29 +0200

    fake update for test webhook

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -40,7 +40,6 @@ app.use(GlobalDirectives)
 app.use(PortalVue)
 app.use(FlatPickr)
 app.use(() => apolloProvider)
-// app.use(VueTimers)
 app.use(vueTransitionsPlugin())
 
 const filters = createFilters(i18n)
